c23340c3fc9af8e5deff05a793fd48e46e3ecdfb
frederic-hallein <frederic.hallein@outlook.com>
2024-12-13 15:46:52 +0100

    add details README

--- README.md
+++ README.md
@@ -10,12 +10,12 @@ initial conditions. The rendering is done using the SDL2 library.
 sudo apt-get install g++
 ```
 
-2. To check which version is installed run the following command:
+$~~~~~~~~$ To check which version is installed run the following command: 
 ```bash
 g++ --version
 ```
 
-3. Install the SDL2 library using the following command:
+2. Install the SDL2 library using the following command:
 ```bash
 sudo apt install libsdl2-image-dev
 ```
@@ -28,15 +28,37 @@ git clone https://github.com/frederic-hallein/double-pendulum
 ```bash
 make all
 ``` 
-This should create a `main` executable file in the same directory. 
+$~~~~~~~~$ This should create a `main` executable file in the same directory. 
 
 6. Run the executable by using the following command: 
 ```bash
 ./main
 ```
 
-7. Set up the initial conditions of the double pendulum or type `d` to run the default values. 
+## Setup
+
+To set up custom initial conditions, simply press `ENTER`. This prompts the user the following variables that require custom input: 
+- `m_1`: mass 1st pendulum (unitless)
+- `m_2`: mass 2nd pendulum (unitless)
+- `L_1`: length 1st pendulum (unitless)
+- `L_2`: length 2nd pendulum (unitless)
+- `ini_theta_1`: initial angle 1st pendulum (in degrees) 
+- `ini_theta_2`: initial angle 2nd pendulum (in degrees)
+- `With friction? (y/n)`: air friction (boolean)
+
+
+To run the default initial conditions, simply run the command `d`. The default setup is the following:
+
+- `m_1 = 1` 
+- `m_2 = 1` 
+- `L_1 = 150` 
+- `L_2 = 150` 
+- `ini_theta_1 = 180`
+- `ini_theta_2 = 180`
+- `friction: no` 
 
 ## Screencast
+
+Screencast of the simulation running the default setup.
 
 [Screencast from 2024-08-06 14-48-29.webm](https://github.com/user-attachments/assets/11727669-aa58-4828-b112-ed4d8f248b12)
--- src/initialization.cpp
+++ src/initialization.cpp
@@ -23,7 +23,7 @@ void initialize(bool& is_initialized, double& m_1, double& m_2, double& L_1, dou
         is_initialized = true;
     }
     else if (input.empty()){
-        std::cout << "Enter mass of disk 1" << std::endl;
+        std::cout << "Enter mass of 1st pendulum" << std::endl;
         std::cout << "m_1 = ";
         while (!(std::cin >> m_1) || m_1 <= 0)
         {
@@ -33,7 +33,7 @@ void initialize(bool& is_initialized, double& m_1, double& m_2, double& L_1, dou
             std::cin.ignore(1000,'\n');
         }
 
-        std::cout << "Enter mass of disk 2" << std::endl;
+        std::cout << "Enter mass of 2nd pendulum" << std::endl;
         std::cout << "m_2 = ";
         while (!(std::cin >> m_2) || m_2 <= 0)
         {
@@ -43,7 +43,7 @@ void initialize(bool& is_initialized, double& m_1, double& m_2, double& L_1, dou
             std::cin.ignore(1000,'\n');
         }
 
-        std::cout << "Enter length of pendulum 1" << std::endl;
+        std::cout << "Enter length of 1st pendulum" << std::endl;
         std::cout << "L_1 = ";
         while (!(std::cin >> L_1) || L_1 <= 0)
         {
@@ -53,7 +53,7 @@ void initialize(bool& is_initialized, double& m_1, double& m_2, double& L_1, dou
             std::cin.ignore(1000,'\n');
         }
 
-        std::cout << "Enter length of pendulum 2" << std::endl;
+        std::cout << "Enter length of 2nd pendulum" << std::endl;
         std::cout << "L_2 = ";
         while (!(std::cin >> L_2) || L_2 <= 0)
         {
@@ -63,7 +63,7 @@ void initialize(bool& is_initialized, double& m_1, double& m_2, double& L_1, dou
             std::cin.ignore(1000,'\n');
         }
 
-        std::cout << "Enter initial angle 1 (in degrees)" << std::endl;
+        std::cout << "Enter initial angle 1st pendulum (in degrees)" << std::endl;
         std::cout << "ini_theta_1 = ";
         while (!(std::cin >> ini_theta_1))
         {
@@ -73,7 +73,7 @@ void initialize(bool& is_initialized, double& m_1, double& m_2, double& L_1, dou
             std::cin.ignore(1000,'\n');
         }
 
-        std::cout << "Enter initial angle 2 (in degrees)" << std::endl;
+        std::cout << "Enter  initial angle 2nd pendulum (in degrees)" << std::endl;
         std::cout << "ini_theta_2 = ";
         while (!(std::cin >> ini_theta_2))
         {
